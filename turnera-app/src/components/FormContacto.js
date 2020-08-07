853046618602369df27b4ce251c9c9041e71b43f
Rslullitel <rslullitel@prokarma.com>
2020-08-07 17:38:33 -0300

    starting front

--- turnera-app/src/components/FormContacto.js
+++ turnera-app/src/components/FormContacto.js
@@ -4,6 +4,7 @@ import Zoom from '@material-ui/core/Zoom';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { FormControl, TextField, Button, Checkbox, FormControlLabel } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
 import { useDispatch } from 'react-redux';
 import { setUserValues } from './features/contacto/userSlice';
 import $, { param } from "jquery"
@@ -90,131 +91,160 @@ const FormContacto = () => {
 	return (
 		<form onSubmit={formik.handleSubmit}>
 			<FormControl>
-				<TextField
-					label="Nro Documento"
-					id="dni"
-					name="dni"
-					type="number"
-					disabled={habilitado}
-					onChange={formik.handleChange}
-					value={formik.values.dni}
-					helperText={formik.errors.dni || "*Campo requerido"}
-					error={formik.errors.dni}
-				/>
-				<TextField 
-					placeholder="Nombre" 
-					label="Nombre"
-					id="nombre"
-					name="nombre"
-					disabled={habilitado}
-					onChange={formik.handleChange}
-					value={formik.values.nombre} 
-					helperText={formik.errors.nombre || "*Campo requerido"}
-					error={formik.errors.nombre}
-				/>
-				<TextField 
-					placeholder="Apellido" 
-					label="Apellido"
-					id="apellido"
-					name="apellido"
-					disabled={habilitado}
-					onChange={formik.handleChange}
-					value={formik.values.apellido}  
-					helperText={formik.errors.apellido || "*Campo requerido"}
-					error={formik.errors.apellido}
-				/>
-				<TextField 
-					placeholder="Email" 
-					label="Email"
-					id="email"
-					name="email"
-					disabled={habilitado}
-					onChange={formik.handleChange}
-					value={formik.values.email} 
-					helperText={formik.errors.email || "*Campo requerido"}
-					error={formik.errors.email}
-				/>
-				<TextField 
-					placeholder="confirmar Email" 
-					label="Confirmar Email"
-					id="confirmarEmail"
-					name="confirmarEmail"
-					disabled={habilitado}
-					onChange={formik.handleChange}
-					value={formik.values.confirmarEmail} 
-					helperText={formik.errors.confirmarEmail || "*Campo requerido"}
-					error={formik.errors.confirmarEmail}
-       			/>
-				<tr>
-					<TextField style={{maxWidth: 75}}
-						type="codArea"
-						placeholder="codArea" 
-						label="Cod. área"
-						id="codArea"
-						name="codArea"
-						disabled={habilitado}
-						onChange={formik.handleChange}
-						value={formik.values.codArea}  
-						helperText={formik.errors.codArea || "*Ingrese Cod. Area sin el 0"}
-						error={formik.errors.codArea} 
-					/>
-					<TextField style={{maxWidth: 130}}
-						value="rigth"
+				<Grid container>
+					<Grid item xs>
+						<TextField
+						label="Nro Documento"
+						id="dni"
+						name="dni"
 						type="number"
-						placeholder="Telefono" 
-						label="Telefono"
-						id="telefono"
-						name="telefono"
 						disabled={habilitado}
 						onChange={formik.handleChange}
-						value={formik.values.telefono}  
-						helperText={formik.errors.telefono || "*Ingrese el numero sin el 15"}
-						error={formik.errors.telefono}
-					/>
-				</tr>
-				<TextField 
-					placeholder="Cuenta Contrato" 
-					label="Cuenta de Servicios"
-					id="cuentaContrato"
-					name="cuentaContrato"
-					disabled={habilitado}
-					onChange={formik.handleChange}
-					value={formik.values.cuentaContrato} 
-					helperText={formik.errors.cuentaContrato}
-					error={formik.errors.cuentaContrato} 
-				/>
-				<Button  
-					onClick={handleToggle}
-					style={{width: 0, height:18}}
-					variant="contained" 
-					color="primary">
-					?
-				</Button>
-				<Zoom in={toggle}>
-					<Paper elevation={4}>
-						{toggle && <img id="service" style={{width: 350, height: 250}} src={service}></img>}
-					</Paper>
-				</Zoom>
-				<Zoom in={!titular}>
-					<TextField 
-						placeholder="Titular de la Cuenta" 
-						label="Titular de la Cuenta"
-						id="titularCuenta"
-						name="titularCuenta"
+						value={formik.values.dni}
+						helperText={formik.errors.dni || "*Campo requerido"}
+						error={formik.errors.dni}
+						/>
+					</Grid>
+					<Grid>
+						<TextField 
+						placeholder="Nombre" 
+						label="Nombre"
+						id="nombre"
+						name="nombre"
 						disabled={habilitado}
 						onChange={formik.handleChange}
-						value={formik.values.titularCuenta} 
-						helperText={formik.errors.titularCuenta}
-						error={formik.errors.titularCuenta} 
+						value={formik.values.nombre} 
+						helperText={formik.errors.nombre || "*Campo requerido"}
+						error={formik.errors.nombre}
+						/>
+					</Grid>
+					<Grid>
+						<TextField 
+						placeholder="Apellido" 
+						label="Apellido"
+						id="apellido"
+						name="apellido"
+						disabled={habilitado}
+						onChange={formik.handleChange}
+						value={formik.values.apellido}  
+						helperText={formik.errors.apellido || "*Campo requerido"}
+						error={formik.errors.apellido}
+						/>
+					</Grid>
+				</Grid>
+				<Grid container>
+					<Grid >
+						<TextField 
+						placeholder="Email" 
+						label="Email"
+						id="email"
+						name="email"
+						disabled={habilitado}
+						onChange={formik.handleChange}
+						value={formik.values.email} 
+						helperText={formik.errors.email || "*Campo requerido"}
+						error={formik.errors.email}
+						/>
+					</Grid>
+					<Grid>
+						<TextField 
+						placeholder="confirmar Email" 
+						label="Confirmar Email"
+						id="confirmarEmail"
+						name="confirmarEmail"
+						disabled={habilitado}
+						onChange={formik.handleChange}
+						value={formik.values.confirmarEmail} 
+						helperText={formik.errors.confirmarEmail || "*Campo requerido"}
+						error={formik.errors.confirmarEmail}
+						/>
+					</Grid>
+				</Grid>
+				<Grid container>
+					<Grid>
+						<TextField 
+							type="codArea"
+							placeholder="codArea" 
+							label="Cod. área"
+							id="codArea"
+							name="codArea"
+							disabled={habilitado}
+							onChange={formik.handleChange}
+							value={formik.values.codArea}  
+							helperText={formik.errors.codArea || "*Ingrese Cod. Area sin el 0"}
+							error={formik.errors.codArea} 
+						/>
+					</Grid>
+					<Grid>
+						<TextField
+							value="rigth"
+							type="number"
+							placeholder="Telefono" 
+							label="Telefono"
+							id="telefono"
+							name="telefono"
+							disabled={habilitado}
+							onChange={formik.handleChange}
+							value={formik.values.telefono}  
+							helperText={formik.errors.telefono || "*Ingrese el numero sin el 15"}
+							error={formik.errors.telefono}
+						/>
+					</Grid>
+				</Grid>
+				<Grid container>
+					<Grid>
+						<TextField 
+						placeholder="Cuenta Contrato" 
+						label="Cuenta de Servicios"
+						id="cuentaContrato"
+						name="cuentaContrato"
+						disabled={habilitado}
+						onChange={formik.handleChange}
+						value={formik.values.cuentaContrato} 
+						helperText={formik.errors.cuentaContrato}
+						error={formik.errors.cuentaContrato} 
 					/>
-				</Zoom>
-				<FormControlLabel
-					disabled={habilitado}
-					value="end"
-					control={<Checkbox color="primary" onClick={handleTitular}/>}
-					label="Soy titular de la cuenta."
-					labelPlacement="end"
-				/>
+					<td>
+					<Button  
+						onClick={handleToggle}
+						style={{width: 0, height:18}}
+						variant="contained" 
+						color="primary">
+						?
+					</Button>
+					</td>
+
+					<Zoom in={toggle}>
+						<Paper elevation={4}>
+							{toggle && <img id="service" style={{width: 350, height: 250}} src={service}></img>}
+						</Paper>
+					</Zoom>
+					</Grid>
+					<Grid>
+					<Zoom in={!titular}>
+						<TextField 
+							placeholder="Titular de la Cuenta" 
+							label="Titular de la Cuenta"
+							id="titularCuenta"
+							name="titularCuenta"
+							disabled={habilitado}
+							onChange={formik.handleChange}
+							value={formik.values.titularCuenta} 
+							helperText={formik.errors.titularCuenta}
+							error={formik.errors.titularCuenta} 
+						/>
+					</Zoom>
+					<FormControlLabel
+						disabled={habilitado}
+						value="end"
+						control={<Checkbox color="primary" onClick={handleTitular}/>}
+						label="Soy titular de la cuenta."
+						labelPlacement="end"
+					/>
+					</Grid>
+				</Grid>
+
+
 				<Button 
 					disabled={habilitado}
 					type="submit" 
