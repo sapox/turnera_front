4ad94a61b922ec66a101afeae003ec8d29525ba8
amadsaed <asaed@prokarma.com>
2020-08-11 12:51:39 -0300

    test

--- turnera-app/src/App.js
+++ turnera-app/src/App.js
@@ -1,38 +1,56 @@
 import React, { Component, useEffect, useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
-import { useDispatch, useSelector } from 'react-redux';
-import { Stepper, Step, StepLabel, StepContent, CardContent } from "@material-ui/core";
-import { MenuItem, Select, FormControl, InputLabel, Button, Paper, Card } from "@material-ui/core";
-import FormContacto from './components/FormContacto';
-import FormTurnos from './components/FormTurnos';
-import TurnoConfirmado from './components/TurnoConfirmado';
-import Disclaimer from './components/Disclaimer';
-import { getTipoCaja } from './api';
-import { setCajaValues } from './components/features/contacto/cajaSlice';
-import { resetUserValues, setUserValues } from './components/features/contacto/userSlice';
-import { resetTurnoValues } from './components/features/contacto/turnoSlice';
-import { resetTurnoConfirmadoValues } from './components/features/contacto/turnoConfirmadoSlice';
-import { resetCajaValues } from './components/features/contacto/cajaSlice';
-import { resetDisclaimer } from './components/features/contacto/disclaimerSlice';
-import FormLogin from './components/FormLogin'
-import swal from 'sweetalert';
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Stepper,
+  Step,
+  StepLabel,
+  StepContent,
+  CardContent,
+} from "@material-ui/core";
+import {
+  MenuItem,
+  Select,
+  FormControl,
+  InputLabel,
+  Button,
+  Paper,
+  Card,
+} from "@material-ui/core";
+import FormContacto from "./components/FormContacto";
+import FormTurnos from "./components/FormTurnos";
+import TurnoConfirmado from "./components/TurnoConfirmado";
+import Disclaimer from "./components/Disclaimer";
+import { getTipoCaja } from "./api";
+import { setCajaValues } from "./components/features/contacto/cajaSlice";
+import {
+  resetUserValues,
+  setUserValues,
+} from "./components/features/contacto/userSlice";
+import { resetTurnoValues } from "./components/features/contacto/turnoSlice";
+import { resetTurnoConfirmadoValues } from "./components/features/contacto/turnoConfirmadoSlice";
+import { resetCajaValues } from "./components/features/contacto/cajaSlice";
+import { resetDisclaimer } from "./components/features/contacto/disclaimerSlice";
+import FormLogin from "./components/FormLogin";
+import swal from "sweetalert";
 import Header from "./components/Header";
 import BuscarTurno from "./components/BuscarTurno";
 import BackOffice from "./components/BackOffice";
 import SingleLogInForm from "./components/SingleLogInForm";
 import BackOfficeLoguado from "./components/BackOfficeLogueado";
 import TablaResultados from "./components/TablaResultados";
+import Container from "@material-ui/core/Container";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     width: "100%",
     "& .MuiTextField-root": {
       margin: theme.spacing(1),
-      width: "25ch"
-    }
+      width: "25ch",
+    },
   },
-  button: { 
+  button: {
     marginTop: theme.spacing(1),
   },
   newTurnButton: {
@@ -41,18 +59,23 @@ const useStyles = makeStyles(theme => ({
     marginRight: theme.spacing(7),
   },
   actionsContainer: {
-    marginBottom: theme.spacing(2)
+    marginBottom: theme.spacing(2),
   },
   resetContainer: {
-    padding: theme.spacing(3)
+    padding: theme.spacing(3),
   },
   cardContainer: {
-    maxWidth: 500,
-  }
+    maxWidth: 5005,
+  },
 }));
 
 function getSteps() {
-  return ["Datos de contacto", "Tipo de tramite", "Confirmar Terminos", "Oficina Comercial"];
+  return [
+    "Datos de contacto",
+    "Tipo de tramite",
+    "Confirmar Terminos",
+    "Oficina Comercial",
+  ];
 }
 
 function getStepContent(step, disclaimer, userStep) {
@@ -62,9 +85,9 @@ function getStepContent(step, disclaimer, userStep) {
     case 1:
       return (userStep && <SelectTipo />) || "Complete información de contacto";
     case 2:
-      return (userStep && <Disclaimer />)  || "Complete el paso anterior";
+      return (userStep && <Disclaimer />) || "Complete el paso anterior";
     case 3:
-      return (disclaimer && <FormTurnos />)  || "Confirme los términos";
+      return (disclaimer && <FormTurnos />) || "Confirme los términos";
     case 4:
       return <TurnoConfirmado />;
     default:
@@ -73,12 +96,12 @@ function getStepContent(step, disclaimer, userStep) {
 }
 
 const SelectTipo = () => {
-  const [ tipo, setTipo ] = React.useState("");
-  const [ tipoDecajas, setTipoDecajas] = useState([]);
-  const [ error, setError ] = useState('');
-  
+  const [tipo, setTipo] = React.useState("");
+  const [tipoDecajas, setTipoDecajas] = useState([]);
+  const [error, setError] = useState("");
+
   const dispatch = useDispatch();
-  const handleChange = event => {
+  const handleChange = (event) => {
     setTipo(event.target.value);
     dispatch(setCajaValues(event.target.value));
   };
@@ -89,26 +112,29 @@ const SelectTipo = () => {
   }
 
   useEffect(() => {
-		try{
-			getTipoCajaFunc();
-		} catch(err){
-			setError(err);
-		}
+    try {
+      getTipoCajaFunc();
+    } catch (err) {
+      setError(err);
+    }
   }, []);
 
   return (
     <FormControl>
       <InputLabel>Seleccionar</InputLabel>
       <Select value={tipo} onChange={handleChange} style={{ minWidth: 150 }}>
-        {tipoDecajas && tipoDecajas.map(tipo => (
-          <MenuItem key={`tipo_${tipo.id}`} value={tipo.id}>{tipo.nombre}</MenuItem>
-        ))}
+        {tipoDecajas &&
+          tipoDecajas.map((tipo) => (
+            <MenuItem key={`tipo_${tipo.id}`} value={tipo.id}>
+              {tipo.nombre}
+            </MenuItem>
+          ))}
       </Select>
     </FormControl>
   );
 };
 
-function Home(){
+function Home() {
   const classes = useStyles();
   const [activeStep, setActiveStep] = React.useState(0);
   const steps = getSteps();
@@ -117,26 +143,28 @@ function Home(){
   const disclaimerStep = useSelector((state) => state.disclaimer.isConfirmed);
   const userStep = useSelector((state) => state.user.submitted);
   const tipoCajaStep = useSelector((state) => state.caja.submitted);
-  const turnoConfirmado = useSelector((state) => state.turnoConfirmado.submitted);
+  const turnoConfirmado = useSelector(
+    (state) => state.turnoConfirmado.submitted
+  );
 
   const handleNext = () => {
-    setActiveStep(prevActiveStep => prevActiveStep + 1);
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
   };
 
   const handleCancel = () => {
-      swal({
-        title: "Cancelar Operacion",
-        text: "¿Esta seguro que desea salir de esta operacion?",
-        icon: "warning",
-        buttons: ["No", "Si"],
-        dangerMode: true
-      }).then((isCanceled) => {
-        if (isCanceled) {
-          handleReset();
-          window.location.reload();
-        }
-      });
-  }
+    swal({
+      title: "Cancelar Operacion",
+      text: "¿Esta seguro que desea salir de esta operacion?",
+      icon: "warning",
+      buttons: ["No", "Si"],
+      dangerMode: true,
+    }).then((isCanceled) => {
+      if (isCanceled) {
+        handleReset();
+        window.location.reload();
+      }
+    });
+  };
 
   const handleReset = () => {
     setActiveStep(0);
@@ -148,107 +176,111 @@ function Home(){
   };
 
   const checkStep = (step) => {
-    if(step === 0 && !userStep){
+    if (step === 0 && !userStep) {
       return true;
-    } else if (step === 1 && !tipoCajaStep){
+    } else if (step === 1 && !tipoCajaStep) {
       return true;
-    } else if (step === 2 && !disclaimerStep){
+    } else if (step === 2 && !disclaimerStep) {
       return true;
-    } else if (step === 3 && !turnoConfirmado){
+    } else if (step === 3 && !turnoConfirmado) {
       return true;
-    }  else {
+    } else {
       return false;
     }
-  }
+  };
 
   return (
-  <div style={{display: 'flex', justifyContent: 'center'}}>
-  <Card className={classes.cardContainer} style={{ marginTop:'30px' }}>
-  <div className={classes.root}>
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      <img
-        src="https://www.aysa.com.ar/assets/Menu/img/logo.png"
-        alt="aysa logo"
-      />  
-    </div>
-
-    
-      <Stepper activeStep={activeStep} orientation="vertical">
-        {steps.map((label, index) => (
-          <Step key={label}>
-            <StepLabel>{label}</StepLabel>
-            <StepContent>
-              <div>{getStepContent(index, disclaimerStep, userStep)}</div>
-              <div className={classes.actionsContainer}>
-                <div>
-                  <Button
-                    onClick={handleCancel}
-                    className={classes.button}
-                  >
-                    Cancelar
-                  </Button>
-                  <Button 
-                    variant="contained"
-                    color="primary"
-                    onClick={handleNext}
-                    className={classes.button}
-                    disabled={checkStep(activeStep)}
-                  >
-                    Siguiente
-                  </Button>
-                </div>
-              </div>
-            </StepContent>
-          </Step>
-        ))}
-      </Stepper>
-   
-    {activeStep === steps.length && (
-      <Paper square elevation={0} className={classes.resetContainer}>
-        <TurnoConfirmado />
-        <Button onClick={handleReset} className={classes.newTurnButton}
+    <div>
+      <Container fixed>
+      <div className={classes.root}>
+          <div style={{ display: "flex"}}>
+            <img
+              src="https://www.aysa.com.ar/assets/Menu/img/logo.png"
+              alt="aysa logo"
+            />
+          </div>
+          <Stepper activeStep={activeStep} orientation="horinzal">
+            {steps.map((label, index) => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+                <StepContent>
+                  <div>{getStepContent(index, disclaimerStep, userStep)}</div>
+                  <div className={classes.actionsContainer}>
+                    <div>
+                      <Button
+                        disabled={activeStep === 0}
+                        onClick={handleCancel}
+                        className={classes.button}
+                      >
+                        Cancelar
+                      </Button>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={handleNext}
+                        className={classes.button}
+                        disabled={checkStep(activeStep)}
+                      >
+                        {activeStep === steps.length - 1
+                          ? "Finish"
+                          : "Siguiente"}
+                      </Button>
+                    </div>
+                  </div>
+                </StepContent>
+              </Step>
+            ))}
+          </Stepper>
+          {activeStep === steps.length && (
+            <Paper square elevation={0} className={classes.resetContainer}>
+              <TurnoConfirmado />
+              <Button
+                onClick={handleReset}
+                className={classes.button}
                 variant="contained"
-                color="primary">
-          Solicitar nuevo turno
-        </Button>
-      </Paper>
-    )}
-  </div>
-</Card>
-</div>
-)}
+                color="primary"
+              >
+                Solicitar nuevo turno
+              </Button>
+            </Paper>
+          )}
+        </div>
+      </Container>
+    </div>
+  );
+}
 
 class App extends Component {
   render() {
     return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Home}>
-          <Home />
-        </Route>
-        <Route path="/turno_confirmado/" exact component={TurnoConfirmado}>
-          <TurnoConfirmado />
-        </Route>
-        <Route path="/backOffice" exact component={BackOffice}>
-          <BackOffice />
-        </Route>
-        <Route path="/buscarTurno" exact component={BuscarTurno}>
-          <BuscarTurno />
-        </Route>
-        <Route path="/login" exact component={FormLogin}>
-          <FormLogin />
-        </Route>
-        <Route path="/singleLoginForm" exact component={SingleLogInForm}>
-          <SingleLogInForm />
-        </Route>
-        <Route path="/BackOfficeL" exact component={BackOfficeLoguado}>
-          <BackOfficeLoguado />
-        </Route>
-        <Route path="/tabla" exact component={TablaResultados}>
-          <TablaResultados />
-        </Route>
-      </Switch>
-    </Router>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home}>
+            <Home />
+          </Route>
+          <Route path="/turno_confirmado/" exact component={TurnoConfirmado}>
+            <TurnoConfirmado />
+          </Route>
+          <Route path="/backOffice" exact component={BackOffice}>
+            <BackOffice />
+          </Route>
+          <Route path="/buscarTurno" exact component={BuscarTurno}>
+            <BuscarTurno />
+          </Route>
+          <Route path="/login" exact component={FormLogin}>
+            <FormLogin />
+          </Route>
+          <Route path="/singleLoginForm" exact component={SingleLogInForm}>
+            <SingleLogInForm />
+          </Route>
+          <Route path="/BackOfficeL" exact component={BackOfficeLoguado}>
+            <BackOfficeLoguado />
+          </Route>
+          <Route path="/tabla" exact component={TablaResultados}>
+            <TablaResultados />
+          </Route>
+        </Switch>
+      </Router>
     );
   }
 }
